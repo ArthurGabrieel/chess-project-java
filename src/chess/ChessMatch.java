b024a32153053095db47a986e8cc2198ea7d9e6d
Arthur Gabriel <arthurelgg@gmail.com>
2022-07-03 00:27:01 -0300

    Printing possive moves with error

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -25,6 +25,12 @@ public class ChessMatch {
         return mat;
     }
 
+    public boolean[][] possibleMoves(ChessPosition sourcePosition){
+        Position position = sourcePosition.toPosition();
+        validadeSourcePosition(position);
+        return board.piece(position).possibleMoves();
+    }
+
     public ChessPiece performChessMove(ChessPosition sourcePosition, ChessPosition targetPosition){
         Position source = sourcePosition.toPosition();
         Position target = targetPosition.toPosition();
@@ -49,7 +55,7 @@ public class ChessMatch {
 
     private void validadeSourcePosition(Position position) {
         if (!board.thereIsAPiece(position)){
-            throw new ChessException("There is no piece on sorce position!");
+            throw new ChessException("There is no piece on source position!");
         }
         if (!board.piece(position).isThereAnyPossibleMove()){
             throw new ChessException("There is no possible moves for the chosen piece!");
